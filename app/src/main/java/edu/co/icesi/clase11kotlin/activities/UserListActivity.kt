ddd3132a7fe1cc13ee236d064ed7c2e24c845ac3
Domiciano <domiciano.rincon@correounivalle.edu.co>
2021-07-01 11:05:58 -0500

    Semana 13

--- app/src/main/java/edu/co/icesi/clase11kotlin/activities/UserListActivity.kt
+++ app/src/main/java/edu/co/icesi/clase11kotlin/activities/UserListActivity.kt
@@ -7,10 +7,8 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.ArrayAdapter
-import android.widget.LinearLayout
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
 
 import co.domi.clase10.model.User
 import com.google.android.gms.tasks.Task
@@ -19,19 +17,18 @@ import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.FirebaseMessaging
 import edu.co.icesi.clase11kotlin.R
-import edu.co.icesi.clase11kotlin.lists.adapter.UserAdapter
 import kotlinx.android.synthetic.main.activity_user_list.*
 import java.util.*
 
-class UserListActivity : AppCompatActivity(), UserAdapter.OnUserClickListener {
+class UserListActivity : AppCompatActivity() {
 
     private var db = FirebaseFirestore.getInstance()
 
     private lateinit var myUser: User
+    private lateinit var userArrayAdapter: ArrayAdapter<User>
+    private lateinit var users: ArrayList<User>
 
     private var isLoggingout: Boolean = false
-
-    private lateinit var adapter: UserAdapter
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,13 +36,9 @@ class UserListActivity : AppCompatActivity(), UserAdapter.OnUserClickListener {
         setContentView(R.layout.activity_user_list)
 
         //Configuracion de la lista
-
-        adapter = UserAdapter()
-        adapter.listener = this
-        userList.adapter = adapter
-        userList.setHasFixedSize(true)
-        val manager = LinearLayoutManager(this)
-        userList.layoutManager = manager
+        users = ArrayList()
+        userArrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, users)
+        userList.adapter = userArrayAdapter
 
         //Recuperar el user de la actividad pasada
         myUser = intent.extras?.getSerializable("myUser") as User
@@ -55,14 +48,16 @@ class UserListActivity : AppCompatActivity(), UserAdapter.OnUserClickListener {
         val userReference = db.collection("users").orderBy("username").limit(10)
         userReference.get().addOnCompleteListener { task: Task<QuerySnapshot> ->
             task.result?.let {
-                adapter.clear()
+                users.clear()
                 for (doc in task.result!!) {
                     val user = doc.toObject(User::class.java)
-                    adapter.addUser(user)
+                    users.add(user)
                 }
+                userArrayAdapter.notifyDataSetChanged()
             }
         }
-
+        //Habilitar los clicks a items de la lista
+        userList.setOnItemClickListener(::onItemClick)
 
         logoutBtn.setOnClickListener(::logout)
 
@@ -81,7 +76,14 @@ class UserListActivity : AppCompatActivity(), UserAdapter.OnUserClickListener {
         finish()
     }
 
-
+    //Se activa cuando damos click a un item de la lista
+    fun onItemClick(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
+        val userClicked = users[position]
+        val intent = Intent(this, ChatActivity::class.java)
+        intent.putExtra("myUser", myUser)
+        intent.putExtra("userClicked", userClicked)
+        startActivity(intent)
+    }
 
     override fun onResume() {
         FirebaseMessaging.getInstance().unsubscribeFromTopic(myUser.username)
@@ -90,19 +92,12 @@ class UserListActivity : AppCompatActivity(), UserAdapter.OnUserClickListener {
     }
 
     override fun onPause() {
-        if (isLoggingout) {
+        if(isLoggingout){
             FirebaseMessaging.getInstance().unsubscribeFromTopic(myUser.username)
-        } else {
+        }else{
             FirebaseMessaging.getInstance().subscribeToTopic(myUser.username)
         }
         super.onPause()
-    }
-
-    override fun onUserClick(userClicked: User) {
-        val intent = Intent(this, ChatActivity::class.java)
-        intent.putExtra("myUser", myUser)
-        intent.putExtra("userClicked", userClicked)
-        startActivity(intent)
     }
 
 }
